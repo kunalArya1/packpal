f07607892f54c3134bb49dfcb27a07dceea6b876
kunalArya1 <kunalkrraj220@gmail.com>
2025-04-30 16:53:02 +0530

    user model added

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/Prisma.js";
+
+export async function POST(req: NextRequest) {
+  // const newUser = await prisma.user.create({
+  //   data: { name: "kunal Arya", email: "kunalkrraj@gmail.com" },
+  // });
+
+  console.log("Hello form the sign up");
+  let userData = await req.json();
+
+  console.log(userData);
+  return NextResponse.json(
+    {
+      message: "kunal Arya",
+    },
+    { status: 200 }
+  );
+}
