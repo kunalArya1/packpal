07448021dce6fc76f034f529193b338bb7471bca
kunalArya1 <kunalkrraj220@gmail.com>
2025-04-30 17:28:26 +0530

    Added Schema validation for auth service

--- src/Schema/AuthShema.ts
+++ src/Schema/AuthShema.ts
@@ -1,6 +1,10 @@
 import { PassThrough } from "stream";
 import { z } from "zod";
 
+/**
+ * Shema Validation for sign in
+ * where we are expecting email and passord
+ */
 export const signInSchema = z.object({
   email: z.string().email({ message: "Email is not valid" }),
   Password: z
@@ -9,7 +13,51 @@ export const signInSchema = z.object({
     .max(17, { message: "Password must not be more than 17 character" }),
 });
 
+/**
+ * Schema Validation for sing up
+ */
 export const signUpSchema = z.object({
-  name: z.string(),
+  fullname: z.string(),
   email: z.string().email({ message: "Email is not valid" }),
+  Password: z
+    .string()
+    .min(6, { message: "Password should be at least 6 character long" })
+    .max(17, { message: "Password must not be more than 17 character" }),
+  Address: z
+    .string()
+    .max(150, { message: "Address must not exceed 150 character" }),
+  CountryCode: z
+    .string()
+    .length(3, { message: "Conutery code should be 3 character long" }),
+  phone: z
+    .string()
+    .length(10, { message: "Phone number should be 10 character long only" }),
+  preferance: z
+    .string()
+    .length(50, { message: "Preferance should not exceed 50 character" }),
+});
+
+/**
+ * Schema Validation for forgot password
+ */
+export const forgotPasswordShema = z.object({
+  email: z.string().email({ message: "Email is not valid" }),
+});
+
+/**
+ * Schema Validation for Reset password
+ */
+export const resetPasswrodSchema = z.object({
+  oldPassword: z
+    .string()
+    .min(6, { message: "Password should be at least 6 character long" })
+    .max(17, { message: "Password must not be more than 17 character" }),
+  newPassword: z
+    .string()
+    .min(6, { message: "Password should be at least 6 character long" })
+    .max(17, { message: "Password must not be more than 17 character" }),
+  confirmPassword: z
+    .string()
+    .min(6, { message: "Password should be at least 6 character long" })
+    .max(17, { message: "Password must not be more than 17 character" }),
 });
